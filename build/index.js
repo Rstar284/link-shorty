d1452e6bc038f4e55f4cd97c20998b1f6fb2d1ec
Rstar284 <supermanrehan284@gmail.com>
2022-02-12 21:50:27 +0400

    fix

--- build/index.js
+++ build/index.js
@@ -22,7 +22,7 @@ const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 app.use(body_parser_1.default.json());
 app.use((0, cors_1.default)());
-app.use(express_1.default.static(path_1.default.join(__dirname, "public")));
+app.use("/", express_1.default.static(path_1.default.join(__dirname, "public")));
 function rand() {
     return (0, crypto_1.randomBytes)(2).toString("hex");
 }
